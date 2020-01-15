f45a934465319d567991f819c020269b331b8cd0
Pantelis <pampatzoglou@gmail.com>
2020-01-15 22:52:01 +0200

    bump

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-./scripts/deploy.sh -t helm -c engineering -a "--set image.tag=${CIRCLE_TAG} --set github.secret=$GITHUB_SECRET --set github.password=$GITHUB_PASSWORD --set github.token=$GITHUB_TOKEN --set mongodb.mongodbRootPassword=$CLA_DB_PASS --set claAssistant.uri=$CLA_DB_URL --name cla ./charts/cla/."
+/scripts/deploy.sh -t helm -c engineering -a "\
+ --set image.tag=${CIRCLE_TAG}\
+ --set github.secret=$GITHUB_SECRET --set github.password=$GITHUB_PASSWORD\
+ --set github.token=$GITHUB_TOKEN \
+ --set mongodb.mongodbRootPassword=$CLA_DB_PASS \
+ --set claAssistant.uri=$CLA_DB_URL \
+ --name cla ./charts/cla/."
