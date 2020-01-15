e9702ebffa9042644f95d8a8cea1e52efb9b7ddd
Pantelis <pampatzoglou@gmail.com>
2020-01-15 22:39:52 +0200

    test run

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/scripts/deploy.sh -t helm -c engineering -a "--set image.tag=${CIRCLE_TAG} --set claAssistant.host=$CLA_ASSISTANT_HOST --set github.secret=$GITHUB_SECRET --set github.password=$GITHUB_PASSWORD --set github.token=$GITHUB_TOKEN cla ./charts/cla/."
+./scripts/deploy.sh -t helm -c engineering -a "--set image.tag=${CIRCLE_TAG} --set github.secret=$GITHUB_SECRET --set github.password=$GITHUB_PASSWORD --set github.token=$GITHUB_TOKEN --set mongodb.mongodbRootPassword=$CLA_DB_PASS --set claAssistant.uri=$CLA_DB_URL --name cla ./charts/cla/."
