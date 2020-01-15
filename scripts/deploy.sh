5cae942c457dda126e30ed0476b9574b39007b5f
Pantelis <pampatzoglou@gmail.com>
2020-01-15 10:51:13 +0200

    enable metrics

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,7 @@ github.token=$GITHUB_TOKEN,\
 mongodb.mongodbRootPassword=$MONGODB_ROOT_PASSWORD,\
 mongodb.mongodbUsername=$MONGODB_USERNAME,\
 mongodb.mongodbPassword=$MONGODB_PASSWORD,\
-mongodb.mongodbDatabase=$MONGODB_DATABASE \
+mongodb.mongodbDatabase=$MONGODB_DATABASE,\
+mongodb.metrics.serviceMonitor.enabled=true,\
+metrics.serviceMonitor.enabled=true,\
 cla ./charts/cla/."
