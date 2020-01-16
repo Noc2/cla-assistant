90ba938ea87346fd39769c0c3fd05bcf0ed12071
Pantelis <pampatzoglou@gmail.com>
2020-01-16 10:52:57 +0200

    metrics enabled + secret credentials

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,11 @@
 
 /scripts/deploy.sh -t helm -c engineering -a "\
  --set image.tag=${CIRCLE_TAG}\
- --set github.secret=$GITHUB_SECRET --set github.password=$GITHUB_PASSWORD\
- --set github.token=$GITHUB_TOKEN \
- --set mongodb.mongodbRootPassword=$CLA_DB_PASS \
- --set claAssistant.uri=$CLA_DB_URL \
+ --set github.client=$GITHUB_CLIENT\
+ --set github.secret=$GITHUB_SECRET\
+ --set github.user=$GITHUB_USER\
+ --set github.token=$GITHUB_TOKEN\
+ --set github.admins=$GITHUB_ADMINS\
+ --set mongodb.mongodbRootPassword=$CLA_DB_PASS\
+ --set claAssistant.uri=$CLA_DB_URL\
  cla ./charts/cla/."
