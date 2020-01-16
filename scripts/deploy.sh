9aeee8d525db204c801a95d96a8462159951f1e0
Pantelis <pampatzoglou@gmail.com>
2020-01-16 18:47:52 +0200

    bump

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,9 @@
  --set github.token=$GITHUB_TOKEN\
  --set github.admins=$GITHUB_ADMINS\
  --set mongodb.mongodbPassword=$CLA_DB_PASS\
+ --set mongodb.mongodbRootPassword=$CLA_DB_PASS\
+ --set mongodb.mongodbDatabase=cla\
+ --set mongodb.mongodbUsername=cla\
  --set claAssistant.uri=$CLA_DB_URL\
  --set metrics.serviceMonitor.enabled=$SERVICE_MONITOR\
  --set mongodb.metrics.serviceMonitor.enabled=$SERVICE_MONITOR\
